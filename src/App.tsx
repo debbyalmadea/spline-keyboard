0bf6398a1ed37641888254e09bcc17be384a402f
Debby Almadea <88815144+debbyalmadea@users.noreply.github.com>
2023-01-25 23:42:25 +0700

    chg: css to be compatible with codesandbox

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,14 @@
 export default function App() {
   return (
-    <div className="w-screen h-screen bg-black relative px-20 py-20">
-      <div className="flex flex-col h-full justify-between">
-        <h1 className="text-[#FFBDBD] font-sans text-[288px] font-bold leading-[240px]">
-          SPLINE KEYBOARD
-        </h1>
-        <p className="w-96 text-white text-4xl font-medium">
+    <div className="container">
+      <div className="text-container">
+        <h1>SPLINE KEYBOARD</h1>
+        <p>
           This is a simple 3D keyboard website made using spline and framer.
         </p>
       </div>
       <iframe
         title="spline-keyboard"
-        className="absolute top-0 right-0 z-10"
         src="https://my.spline.design/hoverinteractioncopy-7647e54c25afdcbf11af6bf41a6c0a5a/"
         frameBorder="0"
         width="100%"
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{html,ts,tsx}"],
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {}
   },
-  plugins: [],
+  plugins: []
 };
